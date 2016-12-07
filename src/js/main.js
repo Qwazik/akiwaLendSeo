3fb5f392234bcf023b3de5f92cf9b0ea31473d9b
qwazik <qwazik21@gmail.com>
2016-12-07 15:27:33 +0300

    final

--- src/js/main.js
+++ src/js/main.js
@@ -37,3 +37,55 @@
         })
     }
   }(jQuery));
+
+  $(function(){
+    pageHeaderHeight();
+    colorTopPositions();
+    starToRequired();
+    $('.fancybox').fancybox({
+        padding: 0,
+        wrapCSS: 'modal-fancy'
+    });
+    $('input[name="phone"]').inputmask('+7(999)-999-99-99');
+    $('input[name="site"]').inputmask('www.a{0,}.a{0,}');
+    $(window).resize(pageHeaderHeight);
+    $('.tarifs__item .big').equalHeight();
+    $('.tarifs__item .desc').equalHeight();
+    var achivementsSlider = $('.achivements-slider').owlCarousel({
+        items: 1,
+        margin: 50,
+        loop: true
+    });
+    $('.achivements-slider__controls a').click(function(){
+        if($(this).is('.prev')){
+            achivementsSlider.trigger('prev.owl.carousel');
+        }else{
+            achivementsSlider.trigger('next.owl.carousel');
+        }
+        return false;
+    });
+
+  });
+
+
+function pageHeaderHeight(){
+    $('.page__header').height($(window).height());
+}
+
+function colorTopPositions(){
+    $('.position-table__position').each(function(){
+        if(+$(this).text() <= 3){
+            $(this).addClass('top');
+        }
+    });
+}
+
+function starToRequired(){
+    $('input').each(function(){
+        if($(this).is(':required')){
+            var span = $(this).closest('label').find('span');
+            $(span).text($(span).text() + '*');
+        }
+    });
+}
+
